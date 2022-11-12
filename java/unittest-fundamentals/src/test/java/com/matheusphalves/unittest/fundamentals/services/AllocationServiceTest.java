e9953ce47bcc4ddf9cf72a221e913f09e2b91e38
matheusphalves <matheusphalves@gmail.com>
2022-11-12 15:30:05 -0300

    uses of assertThat

--- java/unittest-fundamentals/src/test/java/com/matheusphalves/unittest/fundamentals/services/AllocationServiceTest.java
+++ java/unittest-fundamentals/src/test/java/com/matheusphalves/unittest/fundamentals/services/AllocationServiceTest.java
@@ -3,8 +3,9 @@ package com.matheusphalves.unittest.fundamentals.services;
 import com.matheusphalves.unittest.fundamentals.models.Movie;
 import com.matheusphalves.unittest.fundamentals.models.Allocation;
 import com.matheusphalves.unittest.fundamentals.models.User;
-import com.matheusphalves.unittest.fundamentals.utils.DateUtil;
-import org.junit.Assert;
+import static com.matheusphalves.unittest.fundamentals.utils.DateUtil.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java.util.Date;
@@ -16,19 +17,22 @@ public class AllocationServiceTest {
 
         /* Fast, Independent, Repeatable, Self-Verifying, Timely */
 
-        //cenario
+        //scenario
         AllocationService allocationService = new AllocationService();
         User user = new User("Usuario 1");
         Movie movie = new Movie("Filme 1", 2, 5.0);
 
 
-        //acao
+        //action
         Allocation allocation = allocationService.allocateMovie(user, movie);
 
-        //verificacao
-        Assert.assertEquals(5.0, allocation.getAllocationValue(), 0.01);
-        Assert.assertTrue(DateUtil.isSameDate(allocation.getAllocationDate(), new Date()));
-        Assert.assertTrue(DateUtil.isSameDate(allocation.getReturnDate(), DateUtil.getDateWithDaysDiff(1)));
+        //check
+        assertEquals(5.0, allocation.getAllocationValue(), 0.01);
+        assertThat(allocation.getAllocationValue(), is(equalTo(5.0)));
+        assertThat(allocation.getAllocationValue(), is(not(4.0)));
+
+        assertTrue(isSameDate(allocation.getAllocationDate(), new Date()));
+        assertTrue(isSameDate(allocation.getReturnDate(), getDateWithDaysDiff(1)));
 
     }
 
