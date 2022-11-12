87e1fd501b63b4d53c7103e7fe4c656647d8e152
matheusphalves <matheusphalves@gmail.com>
2022-11-12 15:38:50 -0300

    uses of ErrorCollector

--- java/unittest-fundamentals/src/test/java/com/matheusphalves/unittest/fundamentals/services/AllocationServiceTest.java
+++ java/unittest-fundamentals/src/test/java/com/matheusphalves/unittest/fundamentals/services/AllocationServiceTest.java
@@ -6,14 +6,20 @@ import com.matheusphalves.unittest.fundamentals.models.User;
 import static com.matheusphalves.unittest.fundamentals.utils.DateUtil.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
 public class AllocationServiceTest {
 
+    @Rule
+    public ErrorCollector errorCollector = new ErrorCollector();
+
     @Test
-    public void teste() {
+    public void test() {
 
         /* Fast, Independent, Repeatable, Self-Verifying, Timely */
 
@@ -34,6 +40,25 @@ public class AllocationServiceTest {
         assertTrue(isSameDate(allocation.getAllocationDate(), new Date()));
         assertTrue(isSameDate(allocation.getReturnDate(), getDateWithDaysDiff(1)));
 
+    }
+
+    @Test
+    public void testWithErrorCollector(){
+        //scenario
+        AllocationService allocationService = new AllocationService();
+        User user = new User("Usuario 1");
+        Movie movie = new Movie("Filme 1", 2, 5.0);
+
+
+        //action
+        Allocation allocation = allocationService.allocateMovie(user, movie);
+
+        //check
+        errorCollector.checkThat(allocation.getAllocationValue(), is(equalTo(5.0)));
+        errorCollector.checkThat(allocation.getAllocationValue(), is(not(4.0)));
+
+        errorCollector.checkThat(isSameDate(allocation.getAllocationDate(), new Date()), is(false));
+        errorCollector.checkThat(isSameDate(allocation.getReturnDate(), getDateWithDaysDiff(1)), is(false));
     }
 
 }
